e404ce4f2521c269d0fea4e03f3269e768fca69c
Xcode <manishachitti22@gmail.com>
2023-10-12 17:15:01 +0100

    Initial Commit

---
+++ Gitdemo/GitdemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitdemoApp.swift
+//  Gitdemo
+//
+//  Created by Manisha Tumma on 12/10/23.
+//
+
+import SwiftUI
+
+@main
+struct GitdemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
